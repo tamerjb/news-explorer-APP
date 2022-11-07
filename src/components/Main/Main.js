cd8fb6a8eccc7d721f7e295279f7f1ab6677563f
Tamer Jb <tamerjb.98@gmail.com>
2022-11-07 18:57:35 +0200

    added footer

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header/Header';
 import About from '../About/About';
+import Footer from '../Footer/Footer';
 export const Main = () => {
   return (
     <div>
       <Header />
       <About />
+      <Footer />
     </div>
   );
 };
