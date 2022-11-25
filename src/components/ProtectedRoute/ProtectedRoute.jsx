3b3e182c6794050ae43f7fcf80c5432275ad9aa9
Tamer Jb <tamerjb.98@gmail.com>
2022-11-25 16:45:10 +0200

    pre first review

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -10,7 +10,7 @@ export default function ProtectedRoute({ isLoggedIn, children }) {
     if (!isLoggedIn) {
       openPopup();
     }
-  }, [isLoggedIn]);
+  }, [isLoggedIn, openPopup]);
 
   return isLoggedIn ? children : <Navigate to='/' />;
 }
--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
-import './index.css';
 import App from './components/App/App';
-import reportWebVitals from './reportWebVitals';
-import PopupContextProvider from './contexts/PopupsContext';
-import ArticlesContextProvider from './contexts/ArticlesContext';
-import AuthContextProvider from './contexts/CurrentUserContext';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
+import { GlobalProvider } from './globalContext/GlobalContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthContextProvider>
-        <PopupContextProvider>
-          <ArticlesContextProvider>
-            <App />
-          </ArticlesContextProvider>
-        </PopupContextProvider>
-      </AuthContextProvider>
-    </BrowserRouter>
+    <GlobalProvider>
+      <Router>
+        <App />
+      </Router>
+    </GlobalProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
