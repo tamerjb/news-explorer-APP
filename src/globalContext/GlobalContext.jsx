c8a181b486f4523a828ae4f0869b066783cee1f9
Tamer Jb <tamerjb.98@gmail.com>
2022-11-25 16:20:10 +0200

    edited context

---
+++ src/globalContext/GlobalContext.jsx
@@ -0,0 +1,36 @@
+import React, { createContext, useContext } from 'react';
+import {
+  usePopupWithFrom,
+  useUserRegistration,
+  useMobileNav,
+  useTooltip,
+} from './hooks';
+import { useCurrentUser } from './user.hooks';
+import { useCards } from './cards.hooks';
+
+const GlobalContext = createContext({});
+
+export const GlobalProvider = ({ children }) => {
+  const popupWithForm = usePopupWithFrom();
+  const userRegistration = useUserRegistration();
+  const mobileNav = useMobileNav();
+  const tooltip = useTooltip();
+  const currentUser = useCurrentUser();
+  const newsCards = useCards();
+  const store = {
+    popupWithForm,
+    userRegistration,
+    mobileNav,
+    tooltip,
+    currentUser,
+    newsCards,
+  };
+  return (
+    <GlobalContext.Provider value={store}>{children}</GlobalContext.Provider>
+  );
+};
+
+export const useStore = () => {
+  const store = useContext(GlobalContext);
+  return store;
+};
